43281904951b64bfb25f5cc47b986b25661f3b26
k03719010 <k03719010@gmail.com>
2020-02-22 20:54:22 +0900

    画像アップロード機能の追加
    gem carrierwave インスコ　カラム追加　g uploader Picture formでfile_field hmtlに埋め込み

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -69,6 +69,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title, :memo, :score)
+      params.require(:book).permit(:title, :memo, :score, :picture)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_22_112002) do
+ActiveRecord::Schema.define(version: 2020_02_22_114311) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_02_22_112002) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "score"
+    t.string "picture"
   end
 
 end
